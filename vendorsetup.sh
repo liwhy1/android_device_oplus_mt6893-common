06ef92c0c4fa2784ffe4db81fa7cf04584953821
lahaina <nattolecats@gmail.com>
2024-11-04 10:56:20 +0900

    mt6893-common: vendorsetup: Make sure KSU init properly

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,5 +12,8 @@ if ! [ -a $CLANG17 ]; then
     git clone https://gitlab.com/nattolecats/android_prebuilts_clang_host_linux-x86_clang-r487747 prebuilts/clang/host/linux-x86/clang-r487747 --depth 1
 fi
 
+# Make sure KSU init properly
+cd kernel/oplus/mt6893 && git submodule update --init && cd -
+
 # Source patches
 bash device/oplus/mt6893-common/patches/apply.sh

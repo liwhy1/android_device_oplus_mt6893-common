8fadec94e0f9d136b0cc7759f9c0ffa9f8257d48
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-01-30 09:28:29 +0100

    Revert "mt6893-common: Import fingerprint hal from hw/oplus"
    This reverts commit bc2983b54c71cb8efed16876b63652b06ffea4a0.

--- common.mk
+++ common.mk
@@ -152,7 +152,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.mt6893
+    android.hardware.biometrics.fingerprint@2.3-service.oplus
 
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \

15d0ffa13093b54aac27fdcfa89eeddf1c6117fa
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-02-07 16:04:23 +0100

    mt6893-common: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery

--- common.mk
+++ common.mk
@@ -19,8 +19,8 @@ COMMON_PATH := device/oplus/mt6893-common
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+# Disable A/B target features
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 PRODUCT_SHIPPING_API_LEVEL := 30
 

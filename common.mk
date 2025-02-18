2eb1bcbe0b8d2c5c305851f80ddddef849292a3a
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-02-18 23:25:46 +0100

    mt6893-common: Drop OMX
     * also add libutils.vendor and dumpstate to fix some log spam

--- common.mk
+++ common.mk
@@ -250,7 +250,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
-    $(COMMON_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(COMMON_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
     $(COMMON_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(COMMON_PATH)/configs/media/mtk_platform_codecs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_platform_codecs_config.xml
 
@@ -486,7 +486,8 @@ PRODUCT_PACKAGES += \
     libbinder_v32 \
     libhidlbase_v32 \
     libutils_v32 \
-    libstagefright_foundation_v33
+    libstagefright_foundation_v33 \
+    libutils.vendor
 
 # WiFi
 PRODUCT_PACKAGES += \

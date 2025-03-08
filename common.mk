468c917d1a08b51a53e6235aee3fed9549d384a5
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-08 16:49:52 +0100

    Revert: "mt6893-common: Drop OMX"

--- common.mk
+++ common.mk
@@ -250,7 +250,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
-    $(COMMON_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
+    $(COMMON_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(COMMON_PATH)/configs/media/mtk_platform_codecs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_platform_codecs_config.xml
 
@@ -487,8 +487,7 @@ PRODUCT_PACKAGES += \
     libbinder_v32 \
     libhidlbase_v32 \
     libutils_v32 \
-    libstagefright_foundation_v33 \
-    libutils.vendor
+    libstagefright_foundation_v33
 
 # WiFi
 PRODUCT_PACKAGES += \

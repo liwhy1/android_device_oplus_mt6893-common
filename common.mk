2d6f3c1d89b5b2effd938c76e1e48e1ac12ce9a3
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-29 18:21:36 +0100

    mt6893-common: We need this for power too

--- common.mk
+++ common.mk
@@ -447,6 +447,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/oplus \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \

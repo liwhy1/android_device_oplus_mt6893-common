ae201e622db63d676545cc010fc17531e08ea785
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-03-30 16:20:42 +0200

    Revert "mt6893-common: Build old power until new hw/mtk is included"
    This reverts commit 7530ead7ab16d5f273c6ec32869444fb5a494ab8.

--- common.mk
+++ common.mk
@@ -353,7 +353,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr
+    android.hardware.power-service-mediatek
 
 PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub

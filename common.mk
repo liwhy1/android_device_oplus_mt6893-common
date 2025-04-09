77b37e9f9ccae4148d67dd5958253f6714272db8
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-04-09 14:31:07 +0200

    Revert "mt6893-common: Import fp from hw/oplus"
    This reverts commit a3eb022539475bf5e13812e89560343afbd40eb6.

--- common.mk
+++ common.mk
@@ -449,6 +449,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/oplus \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
     $(DEVICE_PATH) \

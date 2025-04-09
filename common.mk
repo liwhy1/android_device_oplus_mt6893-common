67cb934a46bc3db56afa9365cfc41c2ebfcbb2e4
Liwhy <100075328+liwhy1@users.noreply.github.com>
2025-04-09 15:57:54 +0200

    Revert "mt6893-common: Update Shipping API flag"
    This reverts commit 03fbed6898460c317152ddca8313855a6ff40380.

--- common.mk
+++ common.mk
@@ -19,7 +19,7 @@ COMMON_PATH := device/oplus/mt6893-common
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-PRODUCT_SHIPPING_API_LEVEL := 34
+PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Call proprietary blob setup
 $(call inherit-product-if-exists, packages/apps/OneplusParts/parts.mk)

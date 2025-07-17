0b255f0495fa1432cc2fd53155d9a4565f893887
liwhy1 <levi.toth2005@gmail.com>
2025-07-17 03:21:57 +0200

    mt6893-common: Cleanup extrat-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -41,42 +41,39 @@ function blob_fixup {
             "$PATCHELF" --replace-needed libhidlbase.so libhidlbase_v32.so "$2"
             ;;
         vendor/lib*/hw/audio.primary.mt6893.so)
-             "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
-             "${PATCHELF}" --replace-needed "libtinyalsa.so" "libtinyalsa-v32.so" "${2}"
-             ;;
+            "$PATCHELF" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
+            "$PATCHELF" --replace-needed "libtinyalsa.so" "libtinyalsa-v32.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek|vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
 	    [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libcodec2_hidl@1.0.so" "${2}"
-            "${PATCHELF}" --add-needed "libshim.so" "${2}"
-            "${PATCHELF}" --add-needed "libstagefright_foundation_v33.so" "${2}"
+            "$PATCHELF" --replace-needed "libcodec2_hidl@1.0.so" "${2}"
+            "$PATCHELF" --add-needed "libshim.so" "${2}"
+            "$PATCHELF" --add-needed "libstagefright_foundation_v33.so" "${2}"
             ;;
         vendor/bin/mtk_agpsd)
            "$PATCHELF" --replace-needed libcrypto.so libcrypto-v32.so "$2"
            "$PATCHELF" --replace-needed libssl.so libssl-v32.so "${2}"
             ;;
-        vendor/lib*/libkeystore-engine-wifi-hidl.so)
-            "$PATCHELF" --replace-needed android.system.keystore2-V1-ndk_platform.so android.system.keystore2-V1-ndk.so "$2"
-            ;;
         vendor/lib64/libmtkcam_featurepolicy.so)
             # evaluateCaptureConfiguration()
             sed -i "s/\x34\xE8\x87\x40\xB9/\x34\x28\x02\x80\x52/" "$2"
             ;;
-        vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
-            ;;
         vendor/bin/hw/android.hardware.wifi@1.0-service-lazy)
-            "${PATCHELF}" --replace-needed "libwifi-hal.so" "libwifi-hal-mtk.so" "${2}"
+            "$PATCHELF" --replace-needed "libwifi-hal.so" "libwifi-hal-mtk.so" "${2}"
             ;;
         lib64/libem_support_jni.so)
-            "${PATCHELF}" --add-needed "libjni_shim.so" "${2}"
+            "$PATCHELF" --add-needed "libjni_shim.so" "${2}"
             ;;
         vendor/lib64/hw/sensors.mt6893.so)
-            "${PATCHELF}" --add-needed "libsensors_shim.so" "${2}"
+            "$PATCHELF" --add-needed "libsensors_shim.so" "${2}"
+            ;;
+        vendor/bin/hw/mtkfusionrild)
+            "$PATCHELF" --add-needed "libutils-v32.so" "${2}"
             ;;
         vendor/bin/mnld)
             ;&
         vendor/lib64/libaalservice.so)
-            "${PATCHELF}" --replace-needed "libsensorndkbridge.so" "libsensorndkbridge-v30.so" "${2}"
+            "$PATCHELF" --replace-needed "libsensorndkbridge.so" "libsensorndkbridge-v30.so" "${2}"
             ;;
         vendor/lib64/libsource.so)
             grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
@@ -87,14 +84,11 @@ function blob_fixup {
         system_ext/lib64/libsource.so)
             grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
             ;;
-        vendor/lib/libcodec2_vndk-mtk.so)
-            grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
-            ;;
         vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron|vendor/lib*/libnvram.so|odm/bin/hw/vendor.oplus.hardware.charger@1.0-service|vendor/lib64/libsysenv.so)
-             grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
-             ;;
+            grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
+            ;;
         vendor/lib64/hw/hwcomposer.mt6893.so|vendor/lib64/libutils_v32.so)
-             grep -q "libprocessgroup_shim.so" "${2}" || "${PATCHELF}" --add-needed "libprocessgroup_shim.so" "${2}"
+            grep -q "libprocessgroup_shim.so" "${2}" || "${PATCHELF}" --add-needed "libprocessgroup_shim.so" "${2}"
             ;;
     esac
 }
